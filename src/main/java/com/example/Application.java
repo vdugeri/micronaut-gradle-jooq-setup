d6766d7260266898ebb7726d07d23937a2e13092
vdugeri <verem.dugeri@gmail.com>
2024-05-22 16:44:53 +0100

    install spotless
    use version catalogs

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -6,17 +6,20 @@ import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import jakarta.inject.Singleton;
 
-@OpenAPIDefinition(info = @Info(title = "micronaut-gradle-jooq-setup", version = "1.0",
-                                contact = @Contact(name = "micronaut-gradle-jooq-setup",
-                                                   email = "xxxx@xxxx.com")))
+@OpenAPIDefinition(
+        info =
+                @Info(
+                        title = "micronaut-gradle-jooq-setup",
+                        version = "1.0",
+                        contact = @Contact(name = "micronaut-gradle-jooq-setup", email = "xxxx@xxxx.com")))
 @Singleton
 public class Application {
 
-  public static void main(String[] args) {
-    Micronaut.build(args)
-             .mainClass(Application.class)
-             .banner(false)
-             .eagerInitSingletons(true)
-             .start();
-  }
+    public static void main(String[] args) {
+        Micronaut.build(args)
+                .mainClass(Application.class)
+                .banner(false)
+                .eagerInitSingletons(true)
+                .start();
+    }
 }
--- src/main/java/com/example/user/repository/UserRepository.java
+++ src/main/java/com/example/user/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.example.user.repository;
 
+import static com.example.Tables.USERS;
+
 import com.example.tables.pojos.Users;
 import com.example.user.model.UserRequest;
 import jakarta.inject.Singleton;
@@ -8,28 +10,21 @@ import java.util.UUID;
 import org.jooq.DSLContext;
 import reactor.core.publisher.Mono;
 
-import static com.example.Tables.USERS;
-
 @Singleton
 public class UserRepository {
 
-  private final DSLContext dslContext;
+    private final DSLContext dslContext;
 
-  public UserRepository(DSLContext dslContext) {
-    this.dslContext = dslContext;
-  }
+    public UserRepository(DSLContext dslContext) {
+        this.dslContext = dslContext;
+    }
 
-  public Mono<Users> createUser(UserRequest record) {
-    return Mono.from(dslContext.insertInto(USERS)
-                               .columns(USERS.ID,
-                                        USERS.FIRST_NAME,
-                                        USERS.LAST_NAME,
-                                        USERS.CREATED_AT)
-                               .values(UUID.randomUUID(),
-                                       record.firstName(),
-                                       record.lastName(),
-                                       OffsetDateTime.now())
-                               .returningResult(USERS))
-               .map(result -> result.into(Users.class));
-  }
+    public Mono<Users> createUser(UserRequest record) {
+        return Mono.from(dslContext
+                        .insertInto(USERS)
+                        .columns(USERS.ID, USERS.FIRST_NAME, USERS.LAST_NAME, USERS.CREATED_AT)
+                        .values(UUID.randomUUID(), record.firstName(), record.lastName(), OffsetDateTime.now())
+                        .returningResult(USERS))
+                .map(result -> result.into(Users.class));
+    }
 }
